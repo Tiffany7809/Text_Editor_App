74ca356e0645be7d0d7a12586cb6106b11143fd1
Tiffany Dempsey <tiffany.dempsey2000@gmail.com>
2022-07-13 17:55:41 -0700

    registering service worker

--- client/src-sw.js
+++ client/src-sw.js
@@ -29,6 +29,7 @@ registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 // TODO: Implement asset caching
 registerRoute(
   ({ request }) => ["style", "script", "worker"].includes(request.destination),
+
   new StaleWhileRevalidate({
     cacheName: "asset-cache",
     plugins: [
@@ -36,5 +37,6 @@ registerRoute(
         statuses: [0, 200],
       }),
     ],
+    
   })
 );
